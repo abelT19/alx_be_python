09fd27d89059da9ad6e7a5c640ee0577baf813d0
abelT19 <abeltariku43@gmail.com>
2025-09-13 13:42:11 -0700

    Update and rename python_introduction to  basic_operations.py

---
+++ basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum_result = number1 + number2
+print ('sum_result')
+difference_result = number1 - number2
+print ('difference_result')
+product_result = number1 * number2
+print('product_result')
