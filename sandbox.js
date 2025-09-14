b3cd49545334667dba5c838fbcbd11bcaa1922ee
AbelT19 <abeltariku43@gmail.com>
2025-09-14 10:40:03 -0700

    Initial commit: Add project files

---
+++ sandbox.js
@@ -0,0 +1,40 @@
+//let age= 25;
+
+//console.log(age);
+
+//age= 30
+
+//let year=(2025)
+
+//console.log(age, year)
+//let firstname= 'abel';
+//let lastname= 'tariku';
+//let fullname= firstname+ ' '+ lastname
+//console.log(fullname)
+//let email=('abeltarikku42@gmail.com')
+//let result= email.replace('a','b')
+//console.log(result);
+//console.log (25/2)
+//let radius = 10
+//onst pi = 3.14
+//let result = pi * radius**2
+//onsole.log(result);
+///const titele = 'ranissance';
+///const author = 'abel';
+//const like = 10;
+//let result = `The vlog name is ${titele} by ${author} has ${like} likes`;
+//console.log (result);
+//let bedos=[45,87,9,76]
+//console.log(bedos[0])
+//console.log(true,false)
+//
+//let score= '100'
+//console.log(score+1) 
+//const password = 'juhgfdstrf';
+//f(password.length >= 8){
+   //console.log('strong password')
+//}
+const scores = [93, 83, 93, 64, 42, 21, 56];
+for(let i = 0; i < scores.length; i++){
+    console.log('your scores: ',scores[i]);
+}
