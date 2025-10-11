f61dda5fdcdf5818e79542a6c6b656ac2cbed87e
abelT19 <abeltariku43@gmail.com>
2025-10-11 04:46:56 -0700

    book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,19 +1,14 @@
 class Book:
-    # Class variable to count total books
-    total_books = 0
-
     def __init__(self, title, author, year):
         self.title = title
         self.author = author
         self.year = year
-        Book.total_books += 1
 
     def __str__(self):
-        return f"'{self.title}' by {self.author} ({self.year})"
+        return f"{self.title} by {self.author}, published in {self.year}"
 
     def __repr__(self):
-        return f"Book(title='{self.title}', author='{self.author}', year={self.year})"
+        return f"Book('{self.title}', '{self.author}', {self.year})"
 
     def __del__(self):
-        print(f"Book '{self.title}' has been deleted.")
-        Book.total_books -= 1
+        print(f"Deleting {self.title}")
